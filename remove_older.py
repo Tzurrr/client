bed0dd40ab52e022635a108aca77bfd7e988d439
Ubuntu <zur001@gmail.com>
2022-06-12 14:17:27 +0000

    removed the redis section and instead set the redis to expire automatically

--- remove_older.py
+++ remove_older.py
@@ -1,14 +1,11 @@
 import datetime
-import redis
 import os
 
 def remove(array: list):
-    r = redis.Redis()
     for i in array:
         if (datetime.datetime.utcnow() - i[1]) > datetime.timedelta(0, 0, 0, 0, 1, 0, 0):
             array.remove(i)
             try:
-                r.delete(i[0])
                 os.remove(i[0])
             except Exception:
                 pass
