d9806093ffd9b7f9c377849a2e7442fbbc28729c
Ubuntu <zur001@gmail.com>
2022-06-12 11:50:13 +0000

    removed 'dot_finder' references

--- watchdog_client.py
+++ watchdog_client.py
@@ -14,7 +14,6 @@ from watchdog.events import FileCreatedEvent, FileClosedEvent
 import remove_older
 import sender
 import verifier
-import dot_finder
 import elogger
 from concurrent.futures import ProcessPoolExecutor
 from concurrent.futures import as_completed
@@ -32,23 +31,22 @@ def process_queue(q):
         second_half_arr = remove_older.remove(second_half_arr)
         if not q.empty():
             event = q.get()
-            dot = dot_finder.find(event.src_path)
             elogger.write("arrivedtoserver")
 
-            if event.src_path[:dot][-1] != "a" and event.src_path[:dot][-1] != "b":
+            if os.path.splitext(event.src_path)[0][-1] != "a" and os.path.splitext(event.src_path)[0][-1] != "b":
                # print("irrelevant")
                 os.remove(event.src_path)
 
-            elif event.src_path[:dot][-1] == "a":
+            elif os.path.splitext(event.src_path)[0][-1] == "a":
                 #print("first half")
-                r.set(f"{event.src_path[:dot - 2]}", event.src_path)
+                r.set(f"os.path.splitext(event.src_path)[0][:-2]}", event.src_path)
                 first_half_arr.append((event.src_path, datetime.datetime.utcnow()))
 #                if len(first_half_arr) > 0:
  #                   is_valid = verifier.verify(first_half_arr, second_half_arr)
   #                  if is_valid:
    #                     sender.send(event.src_path, second_half_arr)
 
-            elif event.src_path[:dot][-1] == "b":
+            elif os.path.splitext(event.src_path)[0][-1] == "b":
                 #print("second half")
                 second_half_arr.append((event.src_path, datetime.datetime.utcnow()))
                 if len(first_half_arr) > 0:
