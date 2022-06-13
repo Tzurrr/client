73a3d6bae414b509a4e16fcce9103739bf664c29
Ubuntu <zur001@gmail.com>
2022-06-13 06:51:15 +0000

    changed the references to the config file parser and to the 'file_to_var' func

--- elogger.py
+++ elogger.py
@@ -4,15 +4,14 @@ import ecs_logging
 import requests
 import uuid
 import os
-import file_to_var
-import config_file_parser
+import json_parser
 
 def write_logs_to_elastic(event_string):
-    conf_dict = config_file_parser.parse()
+    conf_dict = json_parser.parse_json_to_var("/home/tzur/client/config.json")
     url_path = conf_dict["kibanas_url"]
     logger = logging.getLogger("app")
     logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler('/home/tzur/final-client/elvis.json')
+    handler = logging.FileHandler(conf_dict["logfile_path"])
     handler.setFormatter(ecs_logging.StdlibFormatter())
     logger.addHandler(handler)
     json_UUID = uuid.uuid4()
@@ -20,9 +19,9 @@ def write_logs_to_elastic(event_string):
     # TODO: add value check
 
     logger.info(event_string, extra={"http.request.method": "get", "UUID": json_UUID})
-    log_json = file_to_var.json_to_var("/home/tzur/final-client/elvis.json")
+    log_json = json_parser.parse_json_to_var(conf_dict["logfile_path"])
 
-    os.remove("/home/tzur/final-client/elvis.json")
+    os.remove(conf_dict["logfile_path"])
 
     doc_UUID = uuid.uuid4()
     resp = requests.post(url=f"{url_path}/{event_string}/_doc/{doc_UUID}", json=log_json,
