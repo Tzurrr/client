5ccdb3c2fd1f513fcf957f066522a8f44be8ffc0
Ubuntu <zur001@gmail.com>
2022-06-13 12:15:35 +0000

    added exception handling

--- sender.py
+++ sender.py
@@ -6,26 +6,26 @@ import time
 import urllib
 import json_parser
 
-def send_files_to_server(filepath: str, *args):
+def send_files_to_server(filepath: str):
     local_redis = redis.Redis()
-    arr = []
-
     conf_dict = json_parser.parse_json_to_var("/home/tzur/client/config.json")
     url_path = conf_dict["HAProxys_url"]
-    get_val = local_redis.get(f"{os.path.splitext(filepath)[0][:-2]}")
-    if get_val == None:
+    get_val = local_redis.get(f"{os.path.splitext(filepath)[0][:-2]}").decode()
+    if get_val == None or filepath == None or get_val == filepath:
         pass
-    
-    if os.path.splitext(get_val)[0][:-1] == "a":
-        arr = [("files", open(get_val, "rb")), ("files", open(filepath, "rb"))]
-    elif os.path.splitext(get_val)[0][:-1] == "b":
-        arr = [("files", open(file_path, "rb")), ("files", open(get_val, "rb"))]
 
-    resp = requests.post(url=url_path, files=arr)
-    if resp.status_code == 200:
-        elogger.write_logs_to_elastic("sent")
     else:
-        elogger.write_logs_to_elastic("didntsent")
+        if os.path.splitext(filepath)[0][-1] == "a":
+            arr = [("files", open(filepath, "rb")), ("files", open(get_val, "rb"))]
+        elif os.path.splitext(filepath)[0][-1] == "b":
+            arr = [("files", open(get_val, "rb")), ("files", open(filepath, "rb"))]
+
+        resp = requests.post(url=url_path, files=arr)
+        if resp.status_code == 200:
+            elogger.write_logs_to_elastic("sent")
+        else:
+            elogger.write_logs_to_elastic("didntsent")
+
         try:
             os.remove(get_val)
             os.remove(filepath)
