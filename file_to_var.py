9b1bd815285298fe82c3a982b63b4c671b598e5c
Ubuntu <zur001@gmail.com>
2022-06-13 06:51:57 +0000

    Removed because I've merged it with 'config_file_parser'

--- file_to_var.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def json_to_var(json_filename):
-    with open(json_filename, "r") as log_file:
-        log_json = json.load(log_file)
-    return log_json
-
-    raise Exception("file is empty or not found")
-
