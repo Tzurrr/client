cfea8d0566103ae409f3ea53d6187c6e585d5664
Ubuntu <zur001@gmail.com>
2022-06-23 14:30:38 +0000

    Changed the contents sends to elastic when an exception raises

--- json_parser.py
+++ json_parser.py
@@ -6,11 +6,11 @@ import local_logger
 
 def parse_json_to_var(json_filename):
     try:
-        with open(json_filename, "r") as log_file:
+      with open(json_filename, "r") as log_file:
             log_json = json.load(log_file)
-            return log_json            
+            return log_json
     except Exception:
         err_type, value, traceback = sys.exc_info()
-        elogger.write_logs_to_elastic('Error parsering %s: %s' % (value.filename, value.strerror))
+        elogger.write_logs_to_elastic('Error_parsering')
         local_logger.log_to_local_file("filed parsing the conf file's JSON")
 
