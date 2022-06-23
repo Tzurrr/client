9e8b31031278e1cc417bb253f5cf4ffbdcfae71e
Ubuntu <zur001@gmail.com>
2022-06-23 14:28:57 +0000

    added tests dir

---
+++ tests/tests_files/test_json_parser.py
@@ -0,0 +1,24 @@
+import json_parser
+import os
+import pytest
+
+
+class TestJsonParse():
+
+    def test_invalid_file(self):
+        with open("file.json", "w+") as file:
+            name = file.name
+            pass
+
+        with pytest.raises(json_parser.CustomError):
+            json_parser.parse_json_to_var(name)
+
+    def test_valid_file(self):
+        with open("file1.json", "w+") as file:
+            file.write("{}")
+            name = file.name
+            pass
+
+        assert type(json_parser.parse_json_to_var(name)) == dict
+
+
