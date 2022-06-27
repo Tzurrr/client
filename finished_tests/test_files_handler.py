af1fa9b082015e18c9e13406b8a1452730f6abc1
Tzurrr <zur001@gmail.com>
2022-06-27 13:40:07 +0000

    made the test to use the function it tests

--- finished_tests/test_files_handler.py
+++ finished_tests/test_files_handler.py
@@ -1,22 +1,24 @@
-import pytest
 import files_handler
 import redis
 import os
+from watchdog.events import FileClosedEvent
+import files_handler
+from queue import Queue
+import elogger
+import unittest
 
 
-class TestFilesHandler():
-
+class TestFilesHandler(unittest.TestCase):
     def test_save_to_redis(self):
-        # watchdog client should run at the background
         with open("/home/tzur/all-the-photos/abc.txt", "w+") as file:
             name = file.name
 
+        watchdog_queue = Queue()
+        event = FileClosedEvent(name)
+        watchdog_queue.put(event)
+        files_handler.process_queue(watchdog_queue)
+
         r = redis.Redis()
         content = r.get(os.path.splitext(name)[0][:-2]).decode()
-        assert content == "/home/tzur/all-the-photos/abc.txt"
-
-#    def test_with_blank_index_name(self):
- #       with pytest.raises(elogger.BlankIndexException):
-  #          elogger.write_logs_to_elastic("")
-
+        self.assertEqual(content, "/home/tzur/all-the-photos/abc.txt")
 
