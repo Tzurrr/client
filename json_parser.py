749fb257dfdce0329845b904adf13cd77ee8dc0b
Tzurrr <zur001@gmail.com>
2022-06-28 08:41:33 +0000

    Added custom exception and implemented it

--- json_parser.py
+++ json_parser.py
@@ -4,6 +4,12 @@ import sys
 import local_logger
 
 
+class ParseError(Exception):
+    def __init__(self):
+        message = "The contents of the File Wasn't Propriate"
+        super(ParseError, self).__init__(message)
+
+
 def parse_json_to_var(json_filename):
     try:
         with open(json_filename, "r") as log_file:
@@ -13,4 +19,5 @@ def parse_json_to_var(json_filename):
         err_type, value, traceback = sys.exc_info()
         elogger.write_logs_to_elastic('Error parsering %s: %s' % (value.filename, value.strerror))
         local_logger.log_to_local_file("filed parsing the conf file's JSON")
+        raise ParseError
 
